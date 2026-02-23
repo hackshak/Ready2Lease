10316fdd1a4cbb5a3ecffe405054c851af6717de
Shakeeb ur Rehman <msrehman139@gmail.com>
2026-02-23 17:37:34 +0500

    added the upgrade page

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.home_page,name="home")
+    path("",views.home_page,name="home"),
+    path("upgrade/",views.upgrade_page,name="upgrade_page")
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request,"core/home.html")
+
+
+def upgrade_page(request):
+    return render(request,"core/upgrade.html")
