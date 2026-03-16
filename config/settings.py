3ab61a381047ae08afbf1ad6d066cd0ae10b5d8f
Shakeeb ur Rehman <msrehman139@gmail.com>
2026-03-16 19:14:33 +0500

    updated db settings

--- config/settings.py
+++ config/settings.py
@@ -96,29 +96,39 @@ TEMPLATES = [
 
 
 # DATABASE
-if os.getenv("DB_ENGINE") == "mysql":
+if os.getenv("DB_ENGINE") == "postgresql":
     DATABASES = {
-        'default': {
-            'ENGINE':   'django.db.backends.mysql',
-            'NAME':     os.getenv("DB_NAME"),
-            'USER':     os.getenv("DB_USER"),
-            'PASSWORD': os.getenv("DB_PASSWORD"),
-            'HOST':     os.getenv("DB_HOST", "127.0.0.1"),
-            'PORT':     os.getenv("DB_PORT", "3306"),
-            'OPTIONS': {
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-                'charset': 'utf8mb4',
-            },
-            'CONN_MAX_AGE': 60,   # persistent connections (seconds)
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+            "PORT": os.getenv("DB_PORT", "5432"),
+            "CONN_MAX_AGE": 60,
         }
     }
+
+elif os.getenv("DB_ENGINE") == "mysql":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+            "PORT": os.getenv("DB_PORT", "3306"),
+        }
+    }
+
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
+    
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
